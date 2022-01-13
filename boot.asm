27c545a1d258b1d88d9c94a1c6c9d30cf74a9541
Brett Broadhurst <brettbroadhurst@gmail.com>
2022-01-13 08:05:07 -0800

    feat: painting the screen blue

--- boot.asm
+++ boot.asm
@@ -1,27 +1,29 @@
 [org 0x7c00]
 [bits 16]
 
+
 boot:
-	mov ax, 0x0003 ; al = 0x03, ah = 0x00 clears the screen
-	int 0x10       ; invoke video interrupt 
+	; al = 0x13, ah = 0x00 sets the graphics mode to 320x200
+	; allowing for 8-bit color.
+	mov ax, 0x0013
+	int 0x10
 
-	mov ah, 0x0e   ; 'Write character' interrupt code
-	mov si, msg    ; Load the address of the string into SI
+	mov cx, 0
+	mov ax, 0x0c01
+	mov bx, 0x00
 
-.loop:
-	lodsb          ; load DS:SI 
-	cmp al, 0      ; check for NULL terminator
-	jz .done       ; ready to hang
-	mov bh, 0x00   ; page number = 0
-	int 0x10       ; video interrupt
-	jmp .loop
+; paint the whole screen blue
+.paint_blue:
+	cmp cx, 0xfa00 ; 320x200
+	mov dx, cx     ; cx = dx
+	int 0x10
+
+	inc cx
+	jmp .paint_blue
 
 .done:
 	cli            ; do not allow interrupts
 	hlt            ; sleep
-
-msg:
-	db "Hello, world!", 0
 
 times 510 - ($ - $$) db 0 ; padding
 dw 0xaa55                 ; boot sector signature
